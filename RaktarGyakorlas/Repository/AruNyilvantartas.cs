17415a269b0bc1f4d4fe3218fb0e0408f70b05db
9esTerem <9esTerem@ktch.hu>
2026-02-02 18:22:32 +0100

    aru+arunyilvantartas

---
+++ RaktarGyakorlas/Repository/AruNyilvantartas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaktarGyakorlas.Modell;
+
+namespace RaktarGyakorlas.Repository
+{
+    public class AruNyilvantartas
+    {
+        private List<Aru> aruLista;
+
+        public AruNyilvantartas()
+        {
+            aruLista = new List<Aru>();
+            Seeder();
+        }
+        public void Seeder()
+        {
+            var sorok = File.ReadAllLines(@"C:\Data\ekszerek.csv");
+            foreach (var sor in sorok)
+            {
+                aruLista.Add(new Aru(sor));
+            }
+        }
+        public void UjaruFelvesz(string title, string description, decimal price)
+        {
+            
+        }
+        public List<Aru> OsszesAruLekerdez() 
+        {
+            return aruLista;
+        }
+
+        //public Modell.Aru? AruLekerdezIdAlapjan(int id) { }
+        //public Modell.Aru? AruLekerdezTitleAlapjan(string title) { }
+        //public bool AruTorleseIdAlapjan(int id) { }
+        //public bool AruModositasaIdAlapjan(int id, string title, string description, decimal price) { }
+
+    }
+}
